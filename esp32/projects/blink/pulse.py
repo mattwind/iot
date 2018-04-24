61247e391159bcbf5261a6c26dd48d8f3a45c0fc
mattwind <mattwind@gmail.com>
2018-04-24 14:10:42 -0700

    pulse example

---
+++ esp32/projects/blink/pulse.py
@@ -0,0 +1,14 @@
+import machine
+import time
+import math
+
+led = machine.PWM(machine.Pin(2), freq=1000)
+
+def pulse(l, t):
+    for i in range(20):
+        l.duty(int(math.sin(i / 10 * math.pi) * 500 + 500))
+        time.sleep_ms(t)
+
+while True:
+    for i in range(10):
+        pulse(led, 20)
